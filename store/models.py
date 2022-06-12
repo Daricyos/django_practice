1f35565dd8476994bc75a3a5cbd313fd4f73dff8
Daricyos <daricyos@gmail.com>
2022-06-12 13:08:03 +0300

    corrected checkout

--- store/models.py
+++ store/models.py
@@ -23,7 +23,7 @@ class Product(models.Model):
         try:
             url = self.image.url
         except:
-            url = ''
+            url = 'images/placeholder.png'
         return url
 
 class Order(models.Model):
@@ -35,11 +35,28 @@ class Order(models.Model):
     def __str__(self):
         return str(self.id)
 
+    @property
+    def get_cart_total(self):
+        orderitems = self.orderitem_set.all()
+        total = sum([item.get_total for item in orderitems])
+        return total
+        
+    @property
+    def get_cart_items(self):
+        orderitems = self.orderitem_set.all()
+        total = sum([item.quantity for item in orderitems])
+        return total
+
 class OrderItem(models.Model):
     product = models.ForeignKey(Product, on_delete=models.SET_NULL, blank=True, null=True)
     order = models.ForeignKey(Order, on_delete=models.SET_NULL, blank=True, null=True)
-    qunatity = models.IntegerField(default=0, null=True, blank=True)
+    quantity = models.IntegerField(default=0, null=True, blank=True)
     date_added = models.DateTimeField(auto_now_add=True)
+
+    @property
+    def get_total(self):
+        total = self.product.price * self.quantity
+        return total
 
 class ShippingAddress(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, blank=True, null=True)
